4d14eef13034e5e992b4c23e82dea385abeb88a5
Samuel <samuel05.hb@gmail.com>
2026-02-08 13:23:50 -0500

    fix: Fixed folder structure. Removed the components folder within the app directory as that is only used for UI components; not application routes.

--- app/navbar-wrapper.tsx
+++ app/navbar-wrapper.tsx
@@ -9,9 +9,9 @@ export function NavbarWrapper() {
     // Hide the navbar on the login page ("/"), but show it on all other pages.
     if (pathname === "/") return null;
 
-    // TODO (backend theam): This is a temporary solution. We should ideally determine the user's role and render the appropriate navbar based on that,
+    // TODO (backend team): This is a temporary solution. We should ideally determine the user's role and render the appropriate navbar based on that,
     // rather than relying on the URL path. 
-    if (pathname.startsWith("/components/manager-dashboard")) {
+    if (pathname.startsWith("/manager/dashboard")) {
         return <ManagerNavbar />;
     }
 
