9d7bddc0b2f12129d387a9a8583b3290e55f9f78
Samuel <samuel05.hb@gmail.com>
2026-02-08 13:28:22 -0500

    fix: Fixed minor nitpicks.

--- app/navbar-wrapper.tsx
+++ app/navbar-wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { ManagerNavbar } from "@/components/ui/navbars/manager-navabar";
+import { ManagerNavbar } from "@/components/ui/navbars/manager-navbar";
 import { EmployeeNavbar } from "@/components/ui/navbars/employee-navbar";
 
 export function NavbarWrapper() {
@@ -11,7 +11,7 @@ export function NavbarWrapper() {
 
     // TODO (backend team): This is a temporary solution. We should ideally determine the user's role and render the appropriate navbar based on that,
     // rather than relying on the URL path. 
-    if (pathname.startsWith("/manager/dashboard")) {
+    if (pathname.startsWith("/manager")) {
         return <ManagerNavbar />;
     }
 
